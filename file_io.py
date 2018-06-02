f9f593054871d99a2bcdf610a2219d39fed6449f
Brett McGregor <brett.w.mcgregor@gmail.com>
2018-06-02 14:00:45 +1000

    file io

---
+++ file_io.py
@@ -0,0 +1,34 @@
+
+# def copy(file_name, new_file_name):
+#     with open(file_name) as file:
+#         text = file.read()
+#
+#     with open(new_file_name, "w") as new_file:
+#         new_file.write(text[::-1])
+#
+# copy("brett.txt", "reversed.txt")
+
+#
+# def statistics(file_name):
+#     with open(file_name) as file:
+#         lines = file.readlines()
+#
+#     return {
+#         "lines": len(lines),
+#         "words": sum(len(line.split(" ")) for line in lines),
+#         "characters": sum(len(line) for line in lines)}
+#
+#
+# print(statistics("brett.txt"))
+
+
+def find_and_replace(file_name, find_word, replace_word):
+    with open(file_name, mode="w") as file:
+        lines = file.read()
+        new_lines = lines.replace(find_word, replace_word)
+        # file.seek(0)
+        file.write(new_lines)
+
+
+print(find_and_replace("brett.txt", "Google", "Brett Inc"))
+
